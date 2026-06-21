13eb23854209dc64761832da5a3d50f3f2ec3489
ev-Xplorer <devdadarya2005@gmail.com>
2026-06-21 18:07:57 +0530

    Exercise 5 added

--- Deepskilling/PL SQL Programming/Exercise 5 Triggers/Question_1.sql
+++ Deepskilling/PL SQL Programming/Exercise 5 Triggers/Question_1.sql
@@ -2,7 +2,6 @@ CREATE OR REPLACE TRIGGER UpdateCustomerLastModified
 BEFORE UPDATE ON Customers
 FOR EACH ROW
 BEGIN
-    -- Overwrite whatever date was provided with the exact current time
     :NEW.LastModified := SYSDATE;
 END;
 /
--- Deepskilling/PL SQL Programming/Exercise 5 Triggers/Question_2.sql
+++ Deepskilling/PL SQL Programming/Exercise 5 Triggers/Question_2.sql
@@ -2,7 +2,6 @@ CREATE OR REPLACE TRIGGER LogTransaction
 AFTER INSERT ON Transactions
 FOR EACH ROW
 BEGIN
-    -- Insert a record into the AuditLog using the newly generated Transaction ID
     INSERT INTO AuditLog (TransactionID, ActionDate, ActionMessage)
     VALUES (:NEW.TransactionID, SYSDATE, 'A new transaction was successfully processed.');
 END;
