93ae199d4f72d5c8a28ee8914a497a3c632c17fc
ev-Xplorer <devdadarya2005@gmail.com>
2026-06-29 04:10:01 +0530

    Library Management

---
+++ Deepskilling/week 2/Spring Core and Maven/01_Library_Management/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+    public BookRepository() {
+        System.out.println("BookRepository Object is created");
+    }
+
+    public void saveData(){
+        System.out.println("Book data has been saved successfully in database.");
+    }
+}
---
+++ Deepskilling/week 2/Spring Core and Maven/01_Library_Management/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,11 @@
+package com.library.service;
+
+public class BookService{
+    public BookService() {
+        System.out.println("BookService object is create.");
+    }
+
+    public void logNewBook(){
+        System.out.println("processing book business logic in BookService...");
+    }
+}
